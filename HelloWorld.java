184b104889bb59f1dc3550d3a47a4ad1bb9d9396
Jakob Mueller <Jakob Mueller>
2016-01-26 09:57:53 -0700

    Testing

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.printf("Hello Github World\n");
+        System.out.print("Testing...\n");
         // TODO code application logic here
     }
     
