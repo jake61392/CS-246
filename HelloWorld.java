96bcfa87998a2c8013cff710f524d60a441946d4
Jakob Mueller <Jakob Mueller>
2016-01-26 10:01:29 -0700

    Removed world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.printf("Hello Github World\n");
+        System.out.printf("Hello Github\n");
         // TODO code application logic here
     }
     
