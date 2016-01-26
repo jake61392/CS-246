0fda0a80933dd390c035703697a8aecca45bbf73
Jakob Mueller <Jakob Mueller>
2016-01-26 09:30:23 -0700

    Changed output

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.printf("Hello World\n");
+        System.out.printf("Hello Github World\n");
         // TODO code application logic here
     }
     
