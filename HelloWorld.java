109c7c06aa475121ad785cec82d37c9dd4786329
Jakob Mueller <Jakob Mueller>
2016-01-26 10:03:27 -0700

    merged the two

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.printf("Hello Github\n");
+        System.out.printf("Testing...\n")
         // TODO code application logic here
     }
     
